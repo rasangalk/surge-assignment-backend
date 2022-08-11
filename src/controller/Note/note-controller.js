c0766b24b1ae5de39ffec4c5acfd1952880e88a7
Rasanga Lakshith <64822421+rasangalk@users.noreply.github.com>
2022-08-11 13:43:24 +0530

    Create updating a note
    this enables to update the title and the description in a note.
    note title should more than 3 characters and less than 50 characters
    long. also description must have at least 3 characters.

--- src/controller/Note/note-controller.js
+++ src/controller/Note/note-controller.js
@@ -19,3 +19,25 @@ exports.createNote = (req, res) => {
     }
   });
 };
+
+// Note update function
+exports.updateNote = (req, res) => {
+  // Return note id from user's request param
+  const { noteId } = req.params;
+
+  if (noteId) {
+    // Update following fields in the note
+    Note.findOneAndUpdate(
+      { _id: noteId },
+      {
+        title: req.body.title,
+        description: req.body.description,
+      }
+    ).exec((error, result) => {
+      if (error) return res.status(400).json({ error });
+      if (result) {
+        res.status(202).json({ result });
+      }
+    });
+  }
+};
--- src/routes/note-routes.js
+++ src/routes/note-routes.js
@@ -1,8 +1,14 @@
 const express = require("express");
-const { createNote } = require("../controller/Note/note-controller");
+const {
+  createNote,
+  updateNote,
+} = require("../controller/Note/note-controller");
 const router = express.Router();
 
 // Note creation route
 router.post("/note", createNote);
+
+// Note update route
+router.patch("/note/:noteId", updateNote);
 
 module.exports = router;
